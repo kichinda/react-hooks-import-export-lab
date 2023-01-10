82fdf997365a4e49803593665ce70f7a25e3972b
kichinda <kichindajesse@gmail.com>
2023-01-10 14:08:41 +0300

    completed

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,7 @@
 import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
 import App from "../components/App";
+import  user  from "../data/user";
 
 test("it renders without crashing", () => {
   try {
